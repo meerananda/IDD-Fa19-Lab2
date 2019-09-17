e1b2981bfa750bc2152c4cf7001b6278eb3047a0
meerananda <meerananda91@gmail.com>
2019-09-16 20:43:18 -0400

    Update Lowly_Multimeter.ino

--- code/Lowly_Multimeter.ino
+++ code/Lowly_Multimeter.ino
@@ -1,31 +1,13 @@
 /*
-  Analog Input
-
-  Demonstrates analog input by reading an analog sensor on analog pin 0 and
-  turning on and off a light emitting diode(LED) connected to digital pin 13.
-  The amount of time the LED will be on and off depends on the value obtained
-  by analogRead().
-
-  The circuit:
-  - potentiometer
-    center pin of the potentiometer to the analog input 0
-    one side pin (either one) to ground
-    the other side pin to +5V
-  - LED
-    anode (long leg) attached to digital output 13
-    cathode (short leg) attached to ground
-
-  - Note: because most Arduinos have a built-in LED attached to pin 13 on the
-    board, the LED is optional.
-
-  created by David Cuartielles
-  modified 30 Aug 2011
-  By Tom Igoe
-
-  This example code is in the public domain.
-
+  Lowly Multimeter
+  
+  Part of this is example code that is in the public domain.
   http://www.arduino.cc/en/Tutorial/AnalogInput
+
+  References:
+  https://www.arduino.cc/en/Tutorial/ReadAnalogVoltage
 */
+
 // include the library code:
 #include <LiquidCrystal.h>
 
